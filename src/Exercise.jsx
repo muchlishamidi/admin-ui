af660de1fa9753656d89b3575cbea743f65989a9
muchlishamidi <muchlishamidi15163@gmail.com>
2026-04-21 14:12:27 +0700

    es6 (2)

--- src/Exercise.jsx
+++ src/Exercise.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import UserCard from "./UserCard";
+import { getUsers } from "./Services";
 
 function Exercise() {
+  const [users, setUsers] = useState([]);
+  
+  // console.log(getUsers());
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getUsers();
+        setUsers(data);
+      } catch (error) {
+        console.error("[Component] Gagal menampilkan data:", error.message);
+      }
+    };
+    fetchData();
+  }, []);
+
+  // filter untuk User non Semarang
+  // const nonSemarangUsers = users.filter((user) => user.city !== "Semarang");
+
   return (
     <>
       <div className="min-h-screen bg-gray-100 p-6">
@@ -9,24 +29,9 @@ function Exercise() {
           User Cards
         </h1>
         <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6 max-w-5xl mx-auto">
-          <UserCard 
-            name="April"
-            email="april@gmail.com "
-            street="Jl. Imam Bonjol"
-            city="Semarang"
-            />
-          <UserCard 
-            name="Yani"
-            email="yani@gmail.com"
-            street="Jl. Indraprasta"
-            city="Ungaran"
-          />
-          <UserCard 
-            name="Safitri"
-            email="safitri@gmail.com"
-            street="Jl. Pemuda"
-            city="Kendal"
-          />
+          {users.map((user, index) => (
+            <UserCard key={index} {...user} />
+          ))}
         </div>
       </div>
     </>
--- src/UserCard.jsx
+++ src/UserCard.jsx
@@ -1,13 +1,15 @@
 import React, { useState } from 'react'
 
 function UserCard(props) {
-    const {name, email, street, city} = props;
+    const {name, email, street, city, ...rest} = props;
     const [clicked, setClicked] = useState(false);
 
+    console.log(Object.entries(rest));
+
     // fungsi handler
-    function handlelick() {
-        setClicked(true);
-    }
+    // function handlelick() {
+    //     setClicked(true);
+    // }
 
     return (
     <div className="bg-white p-6 rounded-lg shadow hover:shadow-md transition-shadow">
@@ -18,6 +20,14 @@ function UserCard(props) {
         <p className="text-gray-600">
         <span className="font-medium">Address:</span> {street}, {city}
         </p>
+
+        {/* Menampilkan data tambahan dari rest */}
+        {Object.entries(rest).map(([key, value]) => (
+            <p key={key} className="text-gray-600">
+            <span className="font-medium capitalize">{key}:</span> {value}
+            </p>
+        ))}
+
         <button 
         className={`${clicked ? "bg-special-green" : "bg-gray-01"} text-white p-2 rounded-md`}
             onClick={() => setClicked(true)}
