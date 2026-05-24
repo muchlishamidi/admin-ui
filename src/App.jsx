e86a2c17f08368647325789776bed26bdabaa1d2
muchlishamidi <muchlishamidi15163@gmail.com>
2026-05-24 17:15:58 +0700

    feat: add Sign Up Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import "./App.css";
-import SignInPage from "./pages/SignIn";
+import SignUpPage from "./pages/SignUp";
 
 function App() {
   return (
     <>
-      <SignInPage/>
+      <SignUpPage/>
     </>
   );
 }
